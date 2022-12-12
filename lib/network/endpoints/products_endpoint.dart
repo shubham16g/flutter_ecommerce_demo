37621be7898831cea6eeb27db6cbfeb08aab8262
Shubham Gupta <shubham.developer16@gmail.com>
2022-12-12 15:08:59 +0530

    Api endpoint changed

--- lib/network/endpoints/products_endpoint.dart
+++ lib/network/endpoints/products_endpoint.dart
@@ -1,4 +1,3 @@
-import 'package:ecom/models/request/products_request.dart';
 import 'package:ecom/models/response/products_response.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:dio/dio.dart';
@@ -9,6 +8,6 @@ part 'products_endpoint.g.dart';
 abstract class ProductsEndpoint {
   factory ProductsEndpoint(Dio dio, {String baseUrl}) = _ProductsEndpoint;
 
-  @POST('/products')
-  Future<ProductsResponse> getProducts(@Body() ProductsRequest request);
+  @GET('/products/{page}.json')
+  Future<ProductsResponse> getProducts(@Path("page") int page);
 }
